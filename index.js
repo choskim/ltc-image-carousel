4bdd142dc58d14ebd99393fce0fd9e3d82731569
Cho S. Kim <choskim.me@gmail.com>
2015-12-18 11:43:03 -0800

    Add missing semicolons in index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var index = 0;
 var images = []; 
-images[0] = "images/minions-attitude.jpg"
-images[1] = "images/minions-calm.jpg"
-images[2] = "images/minions-focus.jpg"
+images[0] = "images/minions-attitude.jpg";
+images[1] = "images/minions-calm.jpg";
+images[2] = "images/minions-focus.jpg";
 
 var currentImg = document.getElementsByTagName('img')[0];
 currentImg.setAttribute("src", images[index]); 
